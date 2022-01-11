58911a1c0973273aca2abd4c848f3cb3ca1fa86b
Christopher II Lajom <christopheriilajom@gmail.com>
2022-01-11 16:03:13 +0800

    Added buttons, checkboxes, radioboxes, select.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,12 @@ import {MatInputModule} from '@angular/material/input';
     BrowserAnimationsModule,
     FormsModule,
     MatInputModule,
-
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
